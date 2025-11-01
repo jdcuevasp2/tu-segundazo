f5384152f3c321059d152bfe5a818eee47e275da
Juan David Cuevas Perdomo <jd.cuevas2@uniandes.edu.co>
2025-11-01 00:32:11 -0500

    adjustes de test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
@@ -30,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tu-segundazo');
+    expect(compiled.querySelector('h4')?.textContent).toContain('TuSegundazo.com');
   });
 });
--- src/app/vehiculo/vehiculo.spec.ts
+++ src/app/vehiculo/vehiculo.spec.ts
@@ -2,6 +2,6 @@ import { Vehiculo } from './vehiculo';
 
 describe('Vehiculo', () => {
   it('should create an instance', () => {
-    expect(new Vehiculo()).toBeTruthy();
+    expect(new Vehiculo(1, 'Toyota', 'Corolla', 'XEI', 2020, 45000, 'Gris', 'imagen.jpg')).toBeTruthy();
   });
 });
