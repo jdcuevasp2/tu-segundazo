2b2d95ee053a0cd9301d3c38beec715ab0635ec3
Juan David Cuevas Perdomo <jd.cuevas2@uniandes.edu.co>
2025-10-31 22:30:58 -0500

    creación de modulo, servicio y clase de vehículo y componentes para listar vehículo

---
+++ src/app/vehiculo/listar-vehiculo/listar-vehiculo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-vehiculo',
+  templateUrl: './listar-vehiculo.component.html',
+  styleUrl: './listar-vehiculo.component.css'
+})
+export class ListarVehiculoComponent {
+
+}
---
+++ src/app/vehiculo/vehiculo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListarVehiculoComponent } from './listar-vehiculo/listar-vehiculo.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListarVehiculoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VehiculoModule { }
